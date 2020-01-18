e6195f814548feb28fac027172b81a831d06de8a
abdou124 <30547541+abdou124@users.noreply.github.com>
2020-01-18 10:51:37 -0800

    Update views.py
    delete password you may use your account to try to send an email you have to provide both email_id and password for the following variables : fromaddr and psw

--- myapp/views.py
+++ myapp/views.py
@@ -13,30 +13,15 @@ from email import encoders
 from xlsxwriter.workbook import Workbook
 
 
-fromaddr = "testproject086@gmail.com"
+fromaddr = "example@gmail.com"
 toaddrs = "touazi.abdelhak@gmail.com"
-psw = "testproject12"
+psw = ""
 
-posts =[
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27 , 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28 , 2018'
-    }
-]
+
 def home(request):
 
-    context = {
-        'posts': posts
-    }
-    return render(request,"myapp/myapp.html",context)
+
+    return render(request,"myapp/myapp.html")
 
 
 def Form(request):
